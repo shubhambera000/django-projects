fd7a33674833bca7c4071e1545bd161c7670e37b
shubham bera <56145776+shubhambera000@users.noreply.github.com>
2019-10-17 13:38:12 +0530

    Update urls.py

--- friend/urls.py
+++ friend/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='index'),
-    path('login/', views.register_view, name='login'),
+    path('', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('home/', views.home, name='home'),
     path('login/home/', views.home, name='home'),
 ]
